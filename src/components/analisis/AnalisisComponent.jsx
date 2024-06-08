9b83cbe90fe77268e9de1c1b48e8aef2752484ca
Ramiro Cosa <ramirocosa@gmail.com>
2024-06-08 04:09:35 -0300

    stilos de analisis mejorados

--- src/components/analisis/AnalisisComponent.jsx
+++ src/components/analisis/AnalisisComponent.jsx
@@ -1,6 +1,7 @@
 import SearchComponent from "../SearchComponent";
 import LineChartComponent from './LineChartComponent';
 import BarChartComponent from "./BarChartComponent";
+import '../../styles/analisisComponent.css'
 
 const AnalisisComponent = ({ genres, handleChange, genero, dataLine, dataBar }) => {
     
@@ -9,12 +10,16 @@ const AnalisisComponent = ({ genres, handleChange, genero, dataLine, dataBar }) 
 
     return (
         <>
-            <SearchComponent
+        <h2 className="analisis-title">Analisis</h2>
+        <hr />
+        <p htmlFor="" className="select-label">Elije un genero Cinematografico</p>
+            <SearchComponent className="analisis-select"
                 value={genero}
                 handleChange={handleChange}
                 options={genres}
                 placeholder="Seleccionar género"
             />
+            <p className="graph-title">Evolucion Temporal</p>
             <LineChartComponent data={dataLine} />
             <BarChartComponent data={dataBar} />
         </>
